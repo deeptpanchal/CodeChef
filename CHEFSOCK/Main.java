82b9836613cffcf324bc61fd95c3fd067aeee72d
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-04-09 00:43:34 +0530

    Chef and Socks

---
+++ CHEFSOCK/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main 
+{
+	static String solve(int a, int b, int c) 
+	{
+		return (c - a) / b % 2 == 0 ? "Lucky Chef" : "Unlucky Chef";
+	}
+
+	public static void main(String args[]) 
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println(solve(s.nextInt(), s.nextInt(), s.nextInt()));
+	}
+}
