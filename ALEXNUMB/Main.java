7dbf12af6251908a5df15e55cba5d76e097cdf7b
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-04-08 23:00:27 +0530

    Magic Pairs

---
+++ ALEXNUMB/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main 
+{
+	public static void main(String args[]) 
+	{
+		Scanner s = new Scanner(System.in);
+		int O = s.nextInt();
+		while (O-- > 0) 
+		{
+			int n = s.nextInt();
+			int a[] = new int[n];
+			for (int i = 0; i < n; i++) 
+			{
+				a[i] = s.nextInt();
+			}
+			System.out.println((long) n * (n - 1) / 2);
+		}
+		s.close();
+	}
+}
