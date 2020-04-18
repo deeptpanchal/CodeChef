941dc823d34c484aca35449d37d73efee1d96fd7
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-04-18 19:49:49 +0530

    Chef and Remissness

---
+++ REMISS/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		while (x-- > 0) 
+		{
+			int a = s.nextInt();
+			int b = s.nextInt();
+			System.out.println(Math.max(a, b) + " " + (a + b));
+		}
+		s.close();
+	}
+}
