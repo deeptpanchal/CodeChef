720bc364ef492939eee3e22ec16f8f3fb86cf94c
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-04-18 14:25:05 +0530

    Root of the Problem

---
+++ TREEROOT/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main 
+{
+	public static void main(String args[]) 
+	{
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		while (x-- > 0) 
+		{
+			int n = s.nextInt();
+			int count = 0;
+			for (int i = 0; i < n; i++) 
+			{
+				count += s.nextInt();
+				count -= s.nextInt();
+			}
+			System.out.println(count);
+		}
+		s.close();
+	}
+}
