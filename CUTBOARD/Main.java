ef4c62c62412d2f2c2ee0e9a8f04c4b1ef8c4ee5
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-05-09 16:18:41 +0530

    Cut the Board

---
+++ CUTBOARD/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main 
+{
+	public static void main(String args[]) 
+	{
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		while (x-- > 0) 
+		{
+			int n = s.nextInt();
+			int m = s.nextInt();
+			System.out.println((n - 1) * (m - 1));
+		}
+		s.close();
+	}
+}
