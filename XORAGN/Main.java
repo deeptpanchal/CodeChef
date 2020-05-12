9b8584a483e31cb9fdb7f5b5c8979892f1499ed8
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-05-12 22:17:21 +0530

    Xor Again

---
+++ XORAGN/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main 
+{
+	public static void main(String args[]) 
+	{
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		while (x-- > 0) 
+		{
+			int n = s.nextInt();
+			int ans = 0;
+			for (int i = 0; i < n; i++) 
+			{
+				ans = ans ^ s.nextInt();
+			}
+			System.out.println(ans * 2);
+		}
+		s.close();
+	}
+}
